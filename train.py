7488ee8ecb6322af75c444b12735874b85c72fe9
dungeonpp <praffulpawar4u@gmail.com>
2022-01-15 12:09:55 +0100

    completed utils functions, finishing touches

--- train.py
+++ train.py
@@ -5,13 +5,13 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 from model import UNET
-# from utils import (
-#     load_checkpoint,
-#     save_checkpoint,
-#     get_loaders,
-#     check_accuracy,
-#     save_predictions_as_imgs,
-# )
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_predictions_as_imgs,
+)
 
 # Hyperparameters etc.
 LEARNING_RATE = 1e-4
