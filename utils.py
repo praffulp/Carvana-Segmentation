a4d913eeb767270ddab2de70ca6b7e57ef9a9abe
dungeonpp <praffulpawar4u@gmail.com>
2022-01-15 11:59:25 +0100

    initial commit

---
+++ utils.py
@@ -0,0 +1,19 @@
+import torch
+import torchvision
+from dataset import CarvanaDataset
+from torch.utils.data import DataLoader
+
+def save_checkpoint(state, filename="current_checkpoint.pth.tar"):
+    pass
+
+def load_checkpoint(checkpoint, model):
+    pass
+
+def get_loaders():
+    pass
+
+def check_accuracy():
+    pass
+
+def save_predictions_as_imgs():
+    pass
